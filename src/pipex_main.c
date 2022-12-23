bd49ba444b1307012f5671785f9701536ee05d80
Hei yiu Tsang <htsang@1-B-7.42heilbronn.de>
2022-12-23 23:07:19 +0100

    finished entry point for pipex

---
+++ src/pipex_main.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_main.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/14 14:33:13 by htsang            #+#    #+#             */
+/*   Updated: 2022/12/23 22:58:36 by htsang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "processes/pipex_processes.h"
+
+int main(int argc, const char **argv, const char *envp)
+{
+	t_pipex_states	*states;
+
+	// TODO: argc checker
+	states = init_states(argv, envp);
+	if (!states)
+	{
+		return (EXIT_FAILURE);
+	}
+	fork_command_from_infile(states);
+	while (!check_next_command_is_end(get_parser(states)))
+	{
+		fork_command(states);
+	}
+	fork_command_to_outfile(states);
+	return (EXIT_SUCCESS);
+}
