915b0f22bda35ec3cd06c117d7fe64ec1c93162a
Anthony Tsang <htsang@student.42heilbronn.de>
2023-01-05 01:57:42 +0100

    safe executors using error printers

--- src/processes/safe_executor.c
+++ src/processes/safe_executor.c
@@ -6,7 +6,7 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 18:14:17 by htsang            #+#    #+#             */
-/*   Updated: 2023/01/02 18:48:44 by htsang           ###   ########.fr       */
+/*   Updated: 2023/01/05 01:48:22 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int	safe_open_from_states(int permission, t_pipex_states *states)
 					get_parser(states))), permission, 0644);
 	if (file_fd == -1)
 	{
-		perror(NULL);
+		print_no_file_error(states);
 	}
 	return (file_fd);
 }
@@ -40,12 +40,18 @@ int	safe_open_from_states(int permission, t_pipex_states *states)
 int	safe_execve_from_states(t_pipex_states *states)
 {
 	t_pipex_parser	*parser;
+	char			*unexpanded_executable;
 
 	parser = get_parser(states);
-	expand_executable_path(
-		parse_command(parser));
+	if (parsing_failed(parse_command(parser)))
+	{
+		handle_error(states);
+		return (1);
+	}
+	unexpanded_executable = get_parser_executable(parser);
+	expand_executable_path(parser);
 	execve(get_parser_executable(parser), \
 			get_parser_command(parser), parser->envp);
-	handle_command_not_found_error(states);
+	handle_command_not_found_error(unexpanded_executable, states);
 	return (1);
 }
